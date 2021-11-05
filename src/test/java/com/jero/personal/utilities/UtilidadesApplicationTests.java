91f7d843019232e49e196fd4b1ac24938c4f9dd8
Jeronimo Rodriguez <jerodriguez@hotelbeds.com>
2021-11-05 12:15:20 +0100

    first commit

---
+++ src/test/java/com/jero/personal/utilities/UtilidadesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jero.personal.utilities;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UtilidadesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
